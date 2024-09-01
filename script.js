931b4c9d96678e4ac272a1b7a7e0ed9788d0193f
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-01 21:17:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+	const form = document.getElementById('form1');
+	const fName = form.elements['fname'].value;
+	const lName = form.elements['lname'].value;
+
+	
+	alert(fName + ' ' + lName);
 
 }
